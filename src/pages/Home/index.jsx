4802c86b98a6b274def3619e506b67d5389a8a4d
Agustin Gomez <agusleongomez@gmail.com>
2022-09-28 22:51:06 -0300

    Fixed styles of home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,23 +4,40 @@ import styles from "./home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.titles}>
-        <div>
+    <>
+      <div className={styles.mobile}>
+        <div className={styles.titles}>
+          <div>
+            <h2>Imagine if</h2>
+            <h2 className={styles.titles__gradient}>Snapchat</h2>
+            <h2>had events</h2>
+          </div>
+          <p className={styles.subtitle}>
+            Easily host and share events with your friends across any social
+            media.
+          </p>
+        </div>
+        <img className={styles.image} src={homeImg} alt="HomeImage" />
+        <Link to="/create">
+          <button className={styles.button}>🎉 Create my event</button>
+        </Link>
+      </div>
+      <div className={styles.desktop}>
+        <img className={styles.image} src={homeImg} alt="HomeImage" />
+        <div className={styles.titles}>
           <h2>Imagine if</h2>
           <h2 className={styles.titles__gradient}>Snapchat</h2>
           <h2>had events</h2>
+          <p className={styles.subtitle}>
+            Easily host and share events with your friends across any social
+            media.
+          </p>
+          <Link to="/create">
+            <button className={styles.button}>🎉 Create my event</button>
+          </Link>
         </div>
-        <p className={styles.subtitle}>
-          Easily host and share events with your friends across any social
-          media.
-        </p>
       </div>
-      <img className={styles.image} src={homeImg} alt="HomeImage" />
-      <Link to="/create">
-        <button className={styles.button}>🎉 Create my event</button>
-      </Link>
-    </div>
+    </>
   );
 };
 
